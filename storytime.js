6d6f27e4d77e45bf9f1d189044f4f36ca81ab966
Mike Dreiling <mdarts@gmail.com>
2017-04-29 16:06:10 -0600

    first commit

---
+++ storytime.js
@@ -0,0 +1,151 @@
+var story = [
+
+  {
+    label:"q1",
+    question:"Where were you Friday the 24th? At Home, School, or Church's Chicken?",
+    answers: [
+      {
+        text: "Home",
+        next: "q2"
+      },
+      {
+        text: "School",
+        next: "q3"
+      },
+      {
+        text:"Church's Chicken",
+        next: "q4"
+      }]
+
+  },
+  {
+    label:"q2",
+    question:'What were you doing at Home?',
+    answers: [
+      {
+        text: "Showering",
+        next: "q#"
+      },
+      {
+        text: "Watching Tom and Jerry",
+        next: "q#"
+      },
+      {
+        text:"Plotting a murder",
+        next: "q#"
+      }]
+
+  },
+  {
+    label:"q3",
+    question:'What class were you in?',
+    answers: [
+      {
+        text: "Math",
+        next: "q#"
+      },
+      {
+        text: "French",
+        next: "q#"
+      },
+      {
+        text:"In the hallway eating chicken",
+        next: "q#"
+      }]
+
+  },
+  {
+    label:"q4",
+    question:'Why were you there?',
+    answers: [
+      {
+        text: "BECAUSE I LOVE CHICKEN",
+        next: "q#"
+      },
+      {
+        text: "Because I was really hungry",
+        next: "q#"
+      },
+      {
+        text:"Because I need to ",
+        next: "q#"
+      }]
+
+  },
+  {
+    label:"q5",
+    question:'Congratulations on your incredible hygiene. You are not the murderer.',
+    // answers: [
+    //   {
+    //     text: "Showering",
+    //     next: "q#"
+    //   },
+    //   {
+    //     text: "Watching Tom and Jerry",
+    //     next: "q#"
+    //   },
+    //   {
+    //     text:"Plotting a murder",
+    //     next: "q#"
+    //   }]
+
+  },
+  {
+    label:"q6",
+    question:'Ah sweet. I love Boomerang. Have a donut.',
+    // answers: [
+    //   {
+    //     text: "Math",
+    //     next: "q#"
+    //   },
+    //   {
+    //     text: "French",
+    //     next: "q#"
+    //   },
+    //   {
+    //     text:"In the hallway eating chicken",
+    //     next: "q#"
+    //   }]
+
+  },
+  {
+    label:"q7",
+    question:"Who's murder?",
+    answers: [
+      {
+        text: "Yours",
+        next: "q11"
+      },
+      {
+        text: "Swaggy P",
+        next: "q12"
+      },
+      {
+        text:"The game",
+        next: "q13"
+      }]
+
+  },
+  {
+    label:"q8",
+    question:'Ha. Nerd.',
+  },
+  {
+    label:"q9",
+    question:"C'est formidable!",
+  },
+  {
+    label:"q10",
+    question:"The only chicken joint in town is Church's Chicken!",
+    answers: [
+      {
+        next: "q4"
+      }]
+
+  },
+  {
+    label: "q11",
+
+  }
+
+];
